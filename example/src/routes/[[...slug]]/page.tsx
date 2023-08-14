d02f2ca536f83ea0d2b64d3d68f3c80df0a76e0c
Jirka Svoboda <svobik7@gmail.com>
2023-08-14 12:02:07 +0200

    added catch all tests and example

--- example/src/routes/[[...slug]]/page.tsx
+++ example/src/routes/[[...slug]]/page.tsx
@@ -6,7 +6,20 @@ import { fetchArticles } from 'src/server/db'
 import { router } from 'src/server/router'
 import { getDictionary } from 'src/server/utils/getDictionary'
 
-export default async function BlogPage({ pageHref }: PageProps<void>) {
+export default async function BlogPage({
+  pageHref,
+  params: { slug },
+}: PageProps<{ slug: string[] }>) {
+  // this is an example of using catch all routes under each locale (/en/[[...slug]])
+  if (slug && slug.length > 0) {
+    return (
+      <div>
+        This is catch all route for: <strong>/{slug.join('/')}</strong>. Do
+        whatever you need here.
+      </div>
+    )
+  }
+
   const pageLocale = router.getLocaleFromHref(pageHref)
   const translateArticle = getArticleTranslationFactory(pageLocale)
   const articles = (await fetchArticles()).map(translateArticle)
