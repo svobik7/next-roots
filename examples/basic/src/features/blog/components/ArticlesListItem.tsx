54705a0093acf45a3603d6a9270899512474103f
Jirka Svoboda <svobik7@gmail.com>
2024-11-08 14:40:42 +0100

    ci(actions): added pull request check to github actions

--- examples/basic/src/features/blog/components/ArticlesListItem.tsx
+++ examples/basic/src/features/blog/components/ArticlesListItem.tsx
@@ -32,7 +32,7 @@ export function ArticlesListItem({ article }: ArticlesListItemProps) {
           <Image
             src={`${article.author.avatar}?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80`}
             alt={article.author.username}
-            className="h-10 w-10 rounded-full bg-gray-50"
+            className="size-10 rounded-full bg-gray-50"
             width={40}
             height={40}
           />
--- examples/basic/src/features/common/components/Nav.tsx
+++ examples/basic/src/features/common/components/Nav.tsx
@@ -47,9 +47,9 @@ export function Nav({ items }: NavProps) {
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className="block size-6" aria-hidden="true" />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon className="block size-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
