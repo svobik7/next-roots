d83d26fe1255729ccaa486c86d618e262d547085
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2024-11-29 16:05:15 +0200

    fix(examples-basic): remove code that is dependent of the package itself from i18n file
    Routes generating i18n files cannot depend on the router itself, since the router schema has not
    been generated yet.

--- examples/basic/src/server/utils/getDictionary.ts
+++ examples/basic/src/server/utils/getDictionary.ts
@@ -1,21 +1,6 @@
-import { getPropValue } from 'src/features/common/utils/getPropValue'
-import { fetchDictionary } from '../db'
-import type { Paths } from '../db/types'
 import { getPageLocale } from '../router'
+import { getDictionaryByLocale } from './getDictionaryByLocale'
 
-export async function getDictionary(locale: string = getPageLocale()) {
-  const dictionary = await fetchDictionary(locale)
-
-  if (!dictionary) {
-    throw new Error(`Dictionary does not exists for locale "${locale}".`)
-  }
-
-  return (propName: Paths<typeof dictionary.value>) => {
-    const defaultValue =
-      process.env.NODE_ENV === 'development'
-        ? `MissingTranslation(${locale}): "${propName}"`
-        : propName
-
-    return getPropValue(dictionary.value, propName, defaultValue)
-  }
+export function getDictionary(locale: string = getPageLocale()) {
+  return getDictionaryByLocale(locale)
 }
